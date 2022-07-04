d0938fb0b8c08859ce8bca6995b5f92a98e30027
Gilberto Lupatini <gilberto.lupatini@gmail.com>
2022-07-04 19:34:26 -0300

    Pacote 1 - Bem vindo

---
+++ src/main/java/exemplo01/BemVindo.java
@@ -0,0 +1,17 @@
+package exemplo01;
+
+public class BemVindo {
+
+    public static void main(String[] args){
+        ola("Gilberto");
+    }
+
+    /* Método x
+        Faz
+        ada
+        adas
+    */
+    public static void ola(String nome){
+        System.out.println("Bem vindo: " + nome); //Concatenando strings
+    }
+}
